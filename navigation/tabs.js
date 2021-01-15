1b805cf6886566ea9da75bb94b5d255dc07d107b
Matheus de Lino Ferreira <matheuslino86a@gmail.com>
2021-01-15 13:10:01 -0300

    signup screen finished

---
+++ navigation/tabs.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import {
+    View,
+    Image,
+    TouchabelOpacity,
+    StyleSheet
+} from 'react-native'
---
+++ screens/index.js
@@ -0,0 +1,9 @@
+import Home from './Home'
+import Scan from './Scan'
+import SignUp from './Signup'
+
+export {
+    Home,
+    Scan,
+    SignUp
+}
